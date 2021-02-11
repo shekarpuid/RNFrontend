b2f0961f3c530f8e880d5dcc6222eca8c9e58f7a
Pardu <45798295+rajashekarpilli@users.noreply.github.com>
2021-02-11 13:27:37 +0530

    hoc commit.

---
+++ src/screens/hoc/CounterTwo.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { View, TouchableOpacity } from 'react-native'
+import Text from '../../components/Text'
+import styles from '../../Styles'
+import CounterHOC from './CounterHOC'
+
+const CounterTwo = (props) => {
+    const { count, increment } = props
+
+    return (
+        <>
+            {/* {console.log('counter 2')} */}
+            <View>
+                <TouchableOpacity onPress={() => increment()} style={[styles.button, styles.mb15]}>
+                    <Text style={styles.buttonText}>Clicked {count} times</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    )
+}
+
+export default CounterHOC(CounterTwo)
