3c3722b3b0db1af01464f124fe3d8d09dba945eb
sidsravan <39290179+sidsravan@users.noreply.github.com>
2021-02-11 21:45:53 +0530

    ios status bar commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, Platform } from 'react-native'
+import { SafeAreaView, StatusBar, Platform, View } from 'react-native'
 
 // redux
 import { createStore, applyMiddleware } from 'redux'
@@ -13,11 +13,18 @@ import Routes from './Routes'
 import styles from './Styles'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 44 : StatusBar.currentHeight
 
 const App = () => {
     return (
         <>
-            <StatusBar barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'} backgroundColor="#CC0000" />
+            <View style={{
+                width: "100%",
+                height: STATUS_BAR_HEIGHT,
+                backgroundColor: '#CC0000'
+            }}>
+                <StatusBar barStyle={Platform.OS === 'ios' ? 'light-content' : 'light-content'} backgroundColor="#CC0000" />
+            </View>
             <SafeAreaView style={[styles.safeAreaView]}>
                 <Provider store={store}>
                     <Routes />
