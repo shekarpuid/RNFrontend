b73878df0c7b6aa248351edc1cbd9e550d8240b1
sidsravan <39290179+sidsravan@users.noreply.github.com>
2021-02-11 21:38:33 +0530

    ios status bar commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, Platform } from 'react-native'
+import { SafeAreaView, StatusBar, Platform, View, StyleSheet } from 'react-native'
 
 // redux
 import { createStore, applyMiddleware } from 'redux'
@@ -17,7 +17,11 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 const App = () => {
     return (
         <>
-            <StatusBar barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'} backgroundColor="#CC0000" />
+            {/* <StatusBar barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'} backgroundColor="#CC0000" /> */}
+
+            <View style={styless.StatusBar}>
+                <StatusBar translucent barStyle="light-content" />
+            </View>
             <SafeAreaView style={[styles.safeAreaView]}>
                 <Provider store={store}>
                     <Routes />
@@ -26,5 +30,44 @@ const App = () => {
         </>
     )
 }
+const STATUSBAR_HEIGHT = StatusBar.currentHeight
 
+const styless = StyleSheet.create({
+    StatusBar: {
+        height: Platform.OS === 'ios' ? 44 : STATUSBAR_HEIGHT,
+        backgroundColor: '#CC0000'
+    }
+});
 export default App
+
+// import React, {Component} from "react";
+// import {StyleSheet, StatusBar, View, Platform,Text} from "react-native";
+
+// const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 48 : StatusBar.currentHeight;
+
+// function StatusBarPlaceHolder() {
+//     return (
+//         <View style={{
+//             width: "100%",
+//             height: STATUS_BAR_HEIGHT,
+//             backgroundColor: "blue"
+//         }}>
+//             <StatusBar
+//                 barStyle="light-content"
+//             />
+//         </View>
+//     );
+// }
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <View style={{flex: 1}}>
+//                 <StatusBarPlaceHolder/>
+//                 <Text>...YourContent</Text>
+//             </View>
+//         );
+//     }
+// }
+
+// export default App
