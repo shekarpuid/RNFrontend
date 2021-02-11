e7c66a16935a3f9a134564ce1b202fe0096564ec
Pardu <45798295+rajashekarpilli@users.noreply.github.com>
2021-02-11 12:13:06 +0530

    HOC commit

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { SettingsStackNavigator } from "./StackNavigator";
 import TabNavigator from "./TabNavigator";
+import Counter from "../screens/hoc/Counter";
 
 const Drawer = createDrawerNavigator();
 
@@ -12,6 +13,7 @@ const DrawerNavigator = () => {
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={TabNavigator} />
       <Drawer.Screen name="Settings" component={SettingsStackNavigator} />
+      <Drawer.Screen name="Counter HOC" component={Counter} />
     </Drawer.Navigator>
   );
 }
